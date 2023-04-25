bc82245bfac0fe99c28cc8f01b11bd982365a90c
koomekevo <koome.kevo@gurucrafts.net>
2023-04-25 18:29:42 +0300

    Modify tailwind.config.js to contain screens, pages and componentsdirectories

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
-// tailwind.config.js
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    - content: [],
-    + content: ["./App.{js,jsx,ts,tsx}", "./<custom directory>/**/*.{js,jsx,ts,tsx}"],
-      theme: {
-        extend: {},
-      },
-      plugins: [],
-}
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./screens/**/*.{js,jsx,ts,tsx}",
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
