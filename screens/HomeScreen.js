18f68725daccecbbe5469f8054a433c205e8a166
koomekevo <koome.kevo@gurucrafts.net>
2023-04-26 14:37:59 +0300

    Add Image and Deliver Now Text

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,31 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, Image } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
 
 export default function HomeScreen() {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
   return (
-    <View>
+    <SafeAreaView>
       <Text className="text-red-500">HomeScreen</Text>
-    </View>
-  )
+      {/* Header */}
+      <View>
+        <Image 
+          source={{ 
+            uri: 'https://links.papareact.com/wru'
+          }}
+          className='h-7 w-7 bg-gray-300 p-4 rounded-full'
+        />
+      </View>
+      <View>
+        <Text>Deliver Now</Text>
+      </View>
+    </SafeAreaView>
+  );
 }
