4586688af83a05169f02bedb6e7b73e5ea639f29
koomekevo <koome.kevo@gurucrafts.net>
2023-04-26 20:29:49 +0300

    Add AdjustmentsVerticalIcon

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -6,7 +6,7 @@ import {
   UserIcon,
   ChevronDownIcon,
   SearchIcon,
-  AdjustmentsIcon,
+  AdjustmentsVerticalIcon,
 } from "react-native-heroicons/outline";
 
 export default function HomeScreen() {
@@ -20,23 +20,28 @@ export default function HomeScreen() {
 
   return (
     <SafeAreaView className="bg-white pt-5">
-        {/* Header */}
-        <View className="flex-row pb-3 items-center mx-4 space-x-2">
-          <Image
-            source={{
-              uri: "https://links.papareact.com/wru",
-            }}
-            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
-          />
-          <View className="flex-1">
-            <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
-            <Text className="font-bold text-xl">
-              Current Location
-              <ChevronDownIcon size={20} color="#00CCBB" />
-            </Text>
-          </View>
-          <UserIcon size={35} color="#00CCBB" />
+      {/* Header */}
+      <View className="flex-row pb-3 items-center mx-4 space-x-2">
+        <Image
+          source={{
+            uri: "https://links.papareact.com/wru",
+          }}
+          className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+        />
+        <View className="flex-1">
+          <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+          <Text className="font-bold text-xl">
+            Current Location
+            <ChevronDownIcon size={20} color="#00CCBB" />
+          </Text>
         </View>
+        <UserIcon size={35} color="#00CCBB" />
+      </View>
+      {/* Search */}
+      <View>
+        <View></View>
+        <AdjustmentsVerticalIcon color="#00CCBB" /> 
+      </View>
     </SafeAreaView>
   );
 }
