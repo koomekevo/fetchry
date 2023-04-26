1ee35f0488c1d19b2c856b931a9d8fb11fd82422
koomekevo <koome.kevo@gurucrafts.net>
2023-04-26 14:57:50 +0300

    Add styling for Current Location and Deliver Now

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -13,19 +13,21 @@ export default function HomeScreen() {
 
   return (
     <SafeAreaView>
-      <Text className="text-red-500">HomeScreen</Text>
-      {/* Header */}
-      <View>
-        <Image 
-          source={{ 
-            uri: 'https://links.papareact.com/wru'
-          }}
-          className='h-7 w-7 bg-gray-300 p-4 rounded-full'
-        />
-      </View>
-      <View>
-        <Text>Deliver Now</Text>
-      </View>
+      <Text className="text-red-500">
+        {/* Header */}
+        <View className="flex-row pb-3 items-center mx-4 space-x-2">
+          <Image
+            source={{
+              uri: "https://links.papareact.com/wru",
+            }}
+            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+          />
+          <View>
+            <Text className="font-bold text-gray-400 text-xs">Deliver Now</Text>
+            <Text className="font-bold text-xl">Current Location</Text>
+          </View>
+        </View>
+      </Text>
     </SafeAreaView>
   );
 }
