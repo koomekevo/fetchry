59897c4a50a6cc16867e0e51b8db82061a64836a
koomekevo <koome.kevo@gurucrafts.net>
2023-04-26 17:05:47 +0300

    Upgrade expo

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,7 +2,12 @@ import { View, Text, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
-import { UserIcon, ChevronDownIcon, SearchIcon, AdjustmentsIcon, } from "react-native-heroicons/outline";
+import {
+  UserIcon,
+  ChevronDownIcon,
+  SearchIcon,
+  AdjustmentsIcon,
+} from "react-native-heroicons/outline";
 
 export default function HomeScreen() {
   const navigation = useNavigation();
@@ -14,7 +19,7 @@ export default function HomeScreen() {
   }, []);
 
   return (
-    <SafeAreaView>
+    <SafeAreaView className="bg-white pt-5">
       <Text className="text-red-500">
         {/* Header */}
         <View className="flex-row pb-3 items-center mx-4 space-x-2">
@@ -24,12 +29,14 @@ export default function HomeScreen() {
             }}
             className="h-7 w-7 bg-gray-300 p-4 rounded-full"
           />
-          <View>
-            <Text className="font-bold text-gray-400 text-xs">Deliver Now</Text>
-            <Text className="font-bold text-xl">Current Location
+          <View className="flex-1">
+            <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+            <Text className="font-bold text-xl">
+              Current Location
               <ChevronDownIcon size={20} color="#00CCBB" />
             </Text>
           </View>
+          <UserIcon size={35} color="#00CCBB" />
         </View>
       </Text>
     </SafeAreaView>
